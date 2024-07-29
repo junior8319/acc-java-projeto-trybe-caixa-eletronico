abb8fea68941c49969f3a54e0235cdd6c9d880cf
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-07-29 18:28:03 -0300

    feat-1-test for constructor's method in class Transacao

--- src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
@@ -1,21 +1,33 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/**
+ * The type Transacao test.
+ */
 @DisplayName("Testes dos métodos da classe Transação")
 class TransacaoTest {
 
 
+  /**
+   * Construtor test.
+   */
   @Test
   @DisplayName("1 - Testa o método construtor da classe Transação.")
   void construtorTest() {
-    fail("Não implementado");
-
+    Transacao transacao = new Transacao(10.00, "Depósito inicial.");
+    assertNotNull(transacao);
+    assertEquals(10.00, transacao.getQuantia());
   }
 
 
+  /**
+   * Gets quantia test.
+   */
   @Test
   @DisplayName("2 - Testa o método Getter do atributo quantia.")
   void getQuantiaTest() {
@@ -23,6 +35,9 @@ class TransacaoTest {
 
   }
 
+  /**
+   * Retornar resumo transacao test.
+   */
   @Test
   @DisplayName("3 - Testa o método retornar resumo transação.")
   void retornarResumoTransacaoTest() {
@@ -30,6 +45,9 @@ class TransacaoTest {
 
   }
 
+  /**
+   * Retornar instante test.
+   */
   @Test
   @DisplayName("4 - Testa o método instante está gerando o instante corretamente.")
   void retornarInstanteTest() {
