40746f339d7a62a6112db7d4c9d6c505d6910805
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-07-30 14:42:09 -0300

    feat-6-test for methods adicionarTransacao and retornarSaldo() in the class Conta

--- src/test/java/com/trybe/acc/java/caixaeletronico/ContaTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/ContaTest.java
@@ -6,34 +6,58 @@ import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/**
+ * The type Conta test.
+ */
 @DisplayName("Teste da classe Conta")
 class ContaTest {
 
+  /**
+   * The Mocked client.
+   */
+  PessoaCliente mockedClient = new PessoaCliente(
+      "Pessoa 1",
+      "012.345.678-90",
+      "senhaMuitoDificil"
+  );
+
+  /**
+   * The Mocked bank.
+   */
+  Banco mockedBank = new Banco();
+
+  /**
+   * The Mocked account.
+   */
+  Conta mockedAccount = new Conta("corrente", mockedClient, mockedBank);
+
+  /**
+   * Construtor test.
+   */
   @Test
   @DisplayName("5 - Testa o construtor da classe conta.")
   void construtorTest() {
-    PessoaCliente mockedClient = new PessoaCliente(
-        "Pessoa 1",
-        "012.345.678-90",
-        "senhaMuitoDificil"
-    );
-
-    Banco mockedBank = new Banco();
-
-    Conta mockedAccount = new Conta("corrente", mockedClient, mockedBank);
-
     assertNotNull(mockedAccount);
     assertNotNull(mockedAccount.getIdConta());
     assertEquals(0.00, mockedAccount.retornarSaldo());
   }
 
+  /**
+   * Adicionar transacao test retornar saldo test.
+   */
   @Test
   @DisplayName("6 - Testa o método adicionar transação e retornar saldo da conta.")
   void adicionarTransacaoTestRetornarSaldoTest() {
-    fail("Não implementado");
-
+    assertEquals(0.00, mockedAccount.retornarSaldo());
+    mockedAccount.adicionarTransacao(10.00, "Teste1-adiciona 10.00");
+    assertEquals(10.00, mockedAccount.retornarSaldo());
+    mockedAccount.adicionarTransacao(11.00, "Teste2-adiciona 11.00");
+    assertEquals(21.00, mockedAccount.retornarSaldo());
   }
 
+  /**
+   * Retornar resumo conta test.
+   */
   @Test
   @DisplayName("7 - Testa o método retornar resumo está retornando uma string com os valores corretamente.")
   void retornarResumoContaTest() {
@@ -41,6 +65,9 @@ class ContaTest {
 
   }
 
+  /**
+   * Retornar extrato test.
+   */
   @Test
   @DisplayName("8 - Testa o método retornar extrato está imprimindo os valores corretamente.")
   void retornarExtratoTest() {
@@ -48,6 +75,9 @@ class ContaTest {
 
   }
 
+  /**
+   * Gets id conta test.
+   */
   @Test
   @DisplayName("9 - Testa o método Getter do atributo idConta está retornando.")
   void getIdContaTest() {
@@ -55,6 +85,9 @@ class ContaTest {
 
   }
 
+  /**
+   * Gets pessoa cliente test.
+   */
   @Test
   @DisplayName("10 - Testa o método método Getter do atributo pessoaCliente está retornando.")
   void getPessoaClienteTest() {
