7c315b9a4da788732d33217246437bf71d3283dc
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-08-01 16:51:22 -0300

    feat-23-tests the methods depositar and transferirFundos in class Banco

--- src/test/java/com/trybe/acc/java/caixaeletronico/BancoTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/BancoTest.java
@@ -4,6 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -65,7 +70,69 @@ class BancoTest {
   @Test
   @DisplayName("23 - Testa se o método transferir fundos está transferindo corretamente.")
   void depositarTestTransferirFundosTestmostrarExtratoTest() {
-    fail("Não implementado");
+    Banco mockBank = new Banco();
+
+    PessoaCliente mockClient1 = new PessoaCliente(
+        "Cliente Tst23 Um",
+        "000.000.001-01",
+        "SenhaFácil"
+    );
+
+    Conta mockAccount1 = new Conta("corrente", mockClient1, mockBank);
+    mockClient1.adicionarConta(mockAccount1);
+    Conta mockAccount2 = new Conta("poupança", mockClient1, mockBank);
+    mockClient1.adicionarConta(mockAccount2);
+
+    mockBank.adicionarConta("corrente", mockClient1);
+    mockBank.adicionarConta("poupança", mockClient1);
+    //Refatorar o método construtor de Conta para já executar o método adicionarConta
+    //Motivo: ao criar uma conta bancária, o banco já está mencionado/vinculado.
+    //Também, a execução de adicionarConta é repetitiva e passível de erro dada a assinatura deste
+    //método.
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(outputStream);
+    System.setOut(printStream);
+
+    String instantFormat = "dd/MM/yyyy HH:mm:ss";
+    DateTimeFormatter instantFormatter = DateTimeFormatter.ofPattern(instantFormat);
+    LocalDateTime auxInstant = LocalDateTime.now();
+
+    String instantTransaction1 = instantFormatter.format(auxInstant);
+    mockBank.depositar(mockClient1, 0 , 50.00);
+    mockAccount1.retornarExtrato();
+    String depositarOutput = outputStream.toString();
+
+    String mockedExtract1 = "\nExtrato da conta "
+        + mockAccount1.getIdConta()
+        + "\n\n"
+        + instantTransaction1 + " -------- "
+        + "Depósito recebido" + ": " + "R$ 50,00" + " +\n\n";
+
+    assertEquals(mockedExtract1, depositarOutput);
+
+    mockBank.transferirFundos(mockClient1, 0, 1, 25.00);
+
+    String instantTransaction2 = instantFormatter.format(auxInstant);
+    String mockedExtract2 = "\nExtrato da conta "
+        + mockAccount2.getIdConta()
+        + "\n"
+        + instantTransaction2 + " -------- "
+        + "Transferência recebida" + ": " + "R$ 25,00" + " +\n";
+
+    String mockedExtract3 = "\nExtrato da conta "
+        + mockAccount1.getIdConta()
+        + "\n"
+        + instantTransaction2 + " -------- "
+        + "Transferência enviada" + ": " + "R$ 25,00" + " -\n"
+        + instantTransaction1 + " -------- "
+        + "Depósito recebido" + ": " + "R$ 50,00" + " +\n";
+
+    String extract2 = mockAccount1.retornarExtrato();
+    assertEquals(mockedExtract3, extract2);
+
+    String extract3 = mockAccount2.retornarExtrato();
+    assertEquals(mockedExtract2, extract3);
 
   }
 
@@ -75,7 +142,18 @@ class BancoTest {
   @Test
   @DisplayName("24 - Testa se o método sacar está funcionando corretamente.")
   void depositarTestSacarTestMostrarExtratoTest() {
-    fail("Não implementado");
+    Banco mockBank = new Banco();
+
+    PessoaCliente mockClient1 = new PessoaCliente(
+        "Cliente Tst23 Um",
+        "000.000.001-01",
+        "SenhaFácil"
+    );
+
+    Conta mockAccount1 = new Conta("corrente", mockClient1, mockBank);
+    mockClient1.adicionarConta(mockAccount1);
+
+    mockBank.depositar(mockClient1, 0, 100.00);
 
   }
 
